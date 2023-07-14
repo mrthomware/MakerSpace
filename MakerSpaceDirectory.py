40a21bfa44e9c0795de1650146f2c53b535d1547
Thomas Ware <thomaswareUFV@gmail.com>
2023-07-13 23:22:36 -0700

    run Struct

--- MakerSpaceDirectory.py
+++ MakerSpaceDirectory.py
@@ -9,8 +9,8 @@ def create_directory_structure(readme_file, root_dir):
             # Count the number of '#' to determine the directory level
             level = line.count('#', 0, line.find(' '))
             # Remove '#' and leading/trailing whitespaces, replace spaces with underscores
-            dir_name = line.replace('#', '').strip().replace(' ', '_')
-
+            dir_name = line.replace('#', '').strip().replace(' ', '_').replace(':', '-')
+            
             # Create the directory path based on the level
             dir_path = os.path.join(root_dir, *[''] * (level - 1), dir_name)
 
