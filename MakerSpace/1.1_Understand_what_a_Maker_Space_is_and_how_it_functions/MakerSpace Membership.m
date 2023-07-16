91c19b44b1ca27ef606594df40bb64f6f2096eb6
mrthomware <48370888+mrthomware@users.noreply.github.com>
2023-07-15 17:11:06 -0700

    Create MakerSpace Membership.m

---
+++ MakerSpace/1.1_Understand_what_a_Maker_Space_is_and_how_it_functions/MakerSpace Membership.m
@@ -0,0 +1,42 @@
+# Maker Space Membership
+
+## Overview
+
+Welcome to our Maker Space! This is a collaborative workspace equipped with various tools, technologies, and resources. We aim to foster a culture of innovation, experimentation, and knowledge sharing.
+
+## Why Join Us?
+
+Joining our Maker Space will allow you to:
+
+- Access a wide range of tools and equipment, including 3D printers, laser cutters, CNC machines, and more.
+- Collaborate and network with a vibrant community of makers.
+- Participate in workshops, events, and training sessions.
+- Work on your projects in a supportive and creative environment.
+
+## Membership Options
+
+We offer several membership options to cater to different needs:
+
+- **Monthly Membership:** Ideal for individuals who need regular access to the Maker Space.
+- **Annual Membership:** Best for committed makers looking for a long-term workspace.
+- **Project-Based Membership:** Perfect for individuals working on a specific project.
+
+## How to Become a Member
+
+1. Choose your preferred membership option.
+2. Fill out the membership application form.
+3. Complete the payment process.
+4. Attend an orientation session to familiarize yourself with the space and safety protocols.
+
+## Access and Usage
+
+Once you become a member, you can access the Maker Space during designated hours. You can utilize the tools, equipment, and workstations based on your project needs.
+
+## Safety and Rules
+
+We prioritize safety and have guidelines in place to ensure a secure working environment. All members are required to follow safety protocols and adhere to the rules of the space.
+
+## Conclusion
+
+Join our Maker Space and turn your ideas into reality! For any queries, feel free to contact us.
+
